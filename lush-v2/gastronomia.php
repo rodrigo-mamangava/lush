82d0d7de3043e4276a64f31008f5a4f3c45cc20e
rodrigo-mamangava <rodrigo@mamangava.com>
2016-12-08 20:16:16 -0200

    pag gastronomia
    - primeira versão da pagina gastronomia criada

---
+++ lush-v2/gastronomia.php
@@ -0,0 +1,134 @@
+<?php include './_header.php'; ?>
+<?php include './_menu-principal.php'; ?>
+
+<div class="gastronomia">
+
+
+    <?php
+    $urlImagem = 'img/v2/drink-lush@2x.jpg';
+    $nomeSuite = "Gastronomia";
+    include './_vitrine.php';
+    ?>
+
+    <div class="container faixa-explore-cardapio">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3">
+                <p>
+                    Um exclusivo cardápio gourmet, carta de vinhos, carta de cervejas 
+                    importadas e drinks especiais fazem do Lush uma experiência 
+                    completa e única.
+                </p>
+
+                <a class="btn-explore-cardapio" href="#">EXPLORAR CARDÁPIO</a>
+
+            </div>
+        </div>
+
+    </div><!-- faixa-explore -->
+
+
+    <div class=" faixa-gastronomia-cats">
+        <div class="container">
+
+            <div class="row">
+                
+                <div class="subfaixa-destaque col-xs-12">
+                    <div class="row">
+                        <div class="item-subfaixa">
+                            <div class="col-sm-5 destaque-img">
+                                <img src="img/v2/restaurante@3x.jpg" class="img-destaque">
+                            </div>
+                            <div class=" col-sm-7 destaque-texto">
+
+                                <h3>Restaurante</h3>
+                                <p>
+                                    O LUSH te ajuda a transformar sua comemoração 
+                                    em uma experiência sensorial completa.
+                                </p>
+
+                                <a>CONHEÇA NOSSO ESPAÇO <img src="img/v2/seta@2x.jpg"></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div><!-- subfaixa-destaque -->
+
+                <div class="subfaixa-destaque col-xs-12">
+                    <div class="row">
+                        <div class="item-subfaixa">
+                            <div class=" col-sm-7 destaque-texto">
+
+                                <h3>Bar e Adega</h3>
+                                <p>
+                                    Um exclusivo cardápio gourmet, carta de vinhos,
+                                    carta de cervejas importadas e drinks especiais.
+                                </p>
+
+                                <a>SAIBA MAIS <img src="img/v2/seta@2x.jpg"></a>
+                            </div>
+                            <div class="col-sm-5 destaque-img">
+                                <img src="img/v2/bar-adega@3x.jpg" class="img-destaque">
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div><!-- subfaixa-destaque -->
+
+
+                <div class="subfaixa-destaque col-xs-12">
+                    <div class="row">
+                        <div class="item-subfaixa">
+                            <div class="col-sm-5 destaque-img">
+                                <img src="img/v2/parcerias-especiais@3x.jpg" class="img-destaque">
+                            </div>
+                            <div class=" col-sm-7 destaque-texto">
+
+                                <h3>Parcerias especiais</h3>
+                                <p>
+                                    Os melhores parceiros e fornecedores à disposição 
+                                    para tornar sua experiência gastronômica 
+                                    ainda mais incrível.
+                                </p>
+
+                                <a>NOSSOS PARCEIROS <img src="img/v2/seta@2x.jpg"></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div><!-- subfaixa-destaque -->
+                
+                <div class="subfaixa-destaque col-xs-12">
+                    <div class="row">
+                        <div class="item-subfaixa">
+                            <div class=" col-sm-7 destaque-texto">
+
+                                <h3>Café da manhã e Brunch</h3>
+                                <p>
+                                    Um exclusivo cardápio gourmet, carta de vinhos, 
+                                    carta de cervejas importadas e drinks especiais.
+                                </p>
+
+                                <a>EXPLORAR CARDÁPIO <img src="img/v2/seta@2x.jpg"></a>
+                            </div>
+                            <div class="col-sm-5 destaque-img">
+                                <img src="img/v2/cafedamanha-brunch@3x.jpg" class="img-destaque">
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div><!-- subfaixa-destaque -->
+
+            </div><!-- row -->
+
+
+
+
+        </div>
+    </div><!-- faixa-gastronomia-cats -->
+
+</div>
+
+<?php
+include './_footer.php';
