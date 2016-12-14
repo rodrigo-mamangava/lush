7d8d82e1ada4b3f6b16b7fb177a516c7b769a675
rodrigo-mamangava <rodrigo@mamangava.com>
2016-12-14 16:32:34 -0200

    page experiencias (pacote especiais)

---
+++ lush-v2/experiencia-single.php
@@ -0,0 +1,61 @@
+<?php include './_header.php'; ?>
+<?php include './_menu-principal.php'; ?>
+
+<div class="experiencia-single">
+
+
+    <?php
+    $urlImagem = 'img/v2/drink-lush@2x.jpg';
+    $nomeSuite = "noite romântica";
+    include './_vitrine.php';
+    ?>
+
+    <div class="faixa-texto-simples">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>
+                        O LUSH te ajuda a transformar sua comemoração em uma 
+                        experiência sensorial completa. Acrescente na reserva da 
+                        suíte um dos pacotes abaixo.
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pacote-loop">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="pacote-item col-xs-12 col-sm-10 col-sm-offset-1">
+                    <div class="row">
+                        <div class="col-xs-6 item-texto">
+                            <h3>LUSH SENSATION</h3>
+                            <p class="descricao">
+                                Welcome drinks, jantar (1 entrada + 2 prato principal), 
+                                sobremesa, Chandon e café da manhã LUSH.                             
+                            </p>
+                        </div><!-- item-texto -->
+                        <div class="col-xs-6 item-reserve">
+                            <div class="preco">
+                                <p>
+                                    R$ 375 + SUÍTE ESCOLHIDA
+                                </p>
+                            </div>
+                            <a class="btn-verde-v02">RESERVAR</a>
+
+
+                        </div><!-- item-reserve -->
+                    </div>
+                </div><!-- pacote-item -->
+
+            </div>
+        </div>
+    </div><!-- pacote-loop -->
+
+
+
+</div>
+
+<?php
+include './_footer.php';
--- lush-v2/experiencia.php
+++ lush-v2/experiencia.php
@@ -12,7 +12,7 @@
                     <h2>para presentear</h2>
                     <h3>Lush Gift Card</h3>
                     <div class="row">
-                        <div class="col-sm-5 ">
+                        <div class="col-sm-8 ">
                             <p>
                                 Presenteie com muito estilo e personalidade com 
                                 nossos vales-presente disponíveis em diversos 
@@ -23,11 +23,15 @@
                     </div>
 
                     <a class="btn-verde-v02">COMPRAR AGORA</a>
+                    
+                    <img class="gift-cards" src="img/v2/bitmap@3x.png" >
+                    
 
                 </div><!-- para-presentear -->
             </div> 
 
         </div>
+        <>
     </div><!-- faixa-destaque-->
 
 
@@ -48,7 +52,7 @@
                     <div class="row">
                         <div class="item-subfaixa">
                             <div class="col-sm-5 destaque-img">
-                                <img src="img/v2/restaurante@3x.jpg" class="img-destaque">
+                                <img src="img/v2/love-romantic-date-candlelight-copy@3x.png" class="img-destaque">
                             </div>
                             <div class=" col-sm-7 destaque-texto">
                                 <img class="icon-destaque" src="img/v2/coracao@2x.jpg">
@@ -58,7 +62,7 @@
                                    em uma experiência sensorial completa.
                                 </p>
 
-                                <a>CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
+                                <a href="experiencia-single.php" >CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
                             </div>
                         </div>
                     </div>
@@ -69,17 +73,17 @@
                     <div class="row">
                         <div class="item-subfaixa">
                             <div class=" col-sm-7 destaque-texto">
-                                <img class="icon-destaque" src="img/v2/coracao@2x.jpg">
+                                <img class="icon-destaque" src="img/v2/restaurante@3x.png">
                                 <h3>Jantar Harmonizado</h3>
                                 <p>
                                     Um exclusivo cardápio gourmet, carta de vinhos, 
                                     carta de cervejas importadas e drinks especiais.
                                 </p>
 
-                                <a>CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
+                                <a href="experiencia-single.php">CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
                             </div>
                             <div class="col-sm-5 destaque-img">
-                                <img src="img/v2/bar-adega@3x.jpg" class="img-destaque">
+                                <img src="img/v2/drink-lush-copy@3x.png" class="img-destaque">
                             </div>
 
                         </div>
@@ -92,17 +96,17 @@
                     <div class="row">
                         <div class="item-subfaixa">
                             <div class="col-sm-5 destaque-img">
-                                <img src="img/v2/parcerias-especiais@3x.jpg" class="img-destaque">
+                                <img src="img/v2/love-romantic-date-candlelight-copy-2@3x.png" class="img-destaque">
                             </div>
                             <div class=" col-sm-7 destaque-texto">
-                                <img class="icon-destaque" src="img/v2/coracao@2x.jpg">
+                                <img class="icon-destaque" src="img/v2/chinelo@3x.png">
                                 <h3>Private Spa</h3>
                                 <p>
                                     Relaxe em uma de nossas suítes com piscina 
                                     aquecida privativa, hidromassagem e muito mais.
                                 </p>
 
-                                <a>CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
+                                <a href="experiencia-single.php" >CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
                             </div>
                         </div>
                     </div>
@@ -113,17 +117,17 @@
                     <div class="row">
                         <div class="item-subfaixa">
                             <div class=" col-sm-7 destaque-texto">
-                                <img class="icon-destaque" src="img/v2/coracao@2x.jpg">
+                                <img class="icon-destaque" src="img/v2/cinema@3x.png">
                                 <h3>Noite de Cinema</h3>
                                 <p>
                                     Que tal uma sessão de cinema romântica com 
                                     aquela pessoa especial?
                                 </p>
 
-                                <a>CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
+                                <a href="experiencia-single.php" >CONFIRA OS PACOTES ESPECIAIS <img src="img/v2/seta@2x.jpg"></a>
                             </div>
                             <div class="col-sm-5 destaque-img">
-                                <img src="img/v2/cafedamanha-brunch@3x.jpg" class="img-destaque">
+                                <img src="img/v2/drink-lush-copy-2@3x.png" class="img-destaque">
                             </div>
 
                         </div>
