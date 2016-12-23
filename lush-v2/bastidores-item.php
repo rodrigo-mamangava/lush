05b64051c6e0d81b59813e4a0f16e5dfa1611614
rodrigo-mamangava <rodrigo@mamangava.com>
2016-12-23 16:21:29 -0200

    page bastidores

--- lush-v2/bastidores-item.php
+++ lush-v2/bastidores-item.php
@@ -1,12 +1,16 @@
 <div class="bastidores-item">
-    <a class="" href="bastidores-single.php"> 
-        <div class="row">
-            <div class="col-xs-4 col-sm-2 col-sm-offset-2">
-                <div class="meta-dados">
-                    <p>29 nov 2016</p>
-                </div>
+
+    <div class="row">
+        <div class="col-xs-4 col-sm-2 col-sm-offset-2">
+            <div class="meta-dados">
+                <p>29 nov 2016</p>
             </div>
-            <div class="col-xs-8 col-sm-6 ">
+        </div>
+        <div class="col-xs-8 col-sm-6 ">
+
+            <a href="#"  id="flag-midia" >Lush na mídia</a>
+            <a class="" href="bastidores-single.php">
+
                 <h2>
                     Lá vem a noivinha indica Lush Beer no seu blog.
                 </h2>
@@ -15,6 +19,41 @@
                         Os cervejeiros de plantão têm mais uma razão para visitar o 
                         Lush Motel. Além de uma carta de cervejas especiais, o Lush 
                         apresenta seu mais...
+                    </p>
+                </div>
+            </a>
+
+        </div>
+    </div>
+
+
+</div><!-- bastidores-item -->
+<div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 linha-div">
+        <hr/>
+    </div>
+</div>
+
+<div class="bastidores-item">
+    <a class="" href="bastidores-single.php"> 
+        <div class="row">
+            <div class="col-xs-4 col-sm-2 col-sm-offset-2">
+                <div class="meta-dados">
+                    <p>29 nov 2016</p>
+                </div>
+            </div>
+            <div class="col-xs-8 col-sm-6 ">
+
+                <a href="#" id="flag-essencia" >ESSÊNCIA LUSH</a>
+
+                <h2>
+                    Vogue Brasil recomenda os pacotes especiais do Lush para o Dia dos Namorados
+                </h2>
+                <div class="intro">
+                    <p>
+                        Dia dos namorados: sair da rotina pode ser uma ótima 
+                        pedida, mas reservar o quanto antes é indispensável para 
+                        não deixar a data passar...
                     </p>
                 </div>
 
